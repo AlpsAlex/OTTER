d8c604ed9f309f6ea458084cf0766babd722b7a8
AlpsAlex <jingyao.zhang@ontariotechu.net>
2021-09-29 21:24:27 -0400

    name change

--- projects/Lecture 03 complete/src/main.cpp
+++ projects/Lecture 03 complete/src/main.cpp
@@ -13,7 +13,7 @@ bool initGLFW() {
 	}
 
 	//Create a new GLFW window
-	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
+	window = glfwCreateWindow(800, 800, "Jingyao Zhang 100697498", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	return true;
